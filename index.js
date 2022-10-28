11f061205943de0faf7b696a6bb03229a834387f
Nanzi Mustafa Chowdhury <ge42poj@mytum.de>
2022-10-28 22:20:09 +0600

    dynamic api done

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-const courses = require('./data/courses.json')
+const courses = require('./data/courses.json');
+const details = require('./data/courses.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,6 +14,13 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req,res) =>{
     res.send(courses);
+})
+
+app.get('/courses/:id', (req,res)=>{
+    const id = req.params.id;
+    // res.send(id);
+    const selectedCourse = details.find(d => d.course_id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
